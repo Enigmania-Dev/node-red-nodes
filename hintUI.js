f3f4e3f1cac57a70d1c486b75107819a905f6ea8
Maya Bach <Maya.Bach@edu.rub.de>
2026-06-12 15:47:53 +0200

    hintUI: new Format  expects a list of hint-states in the payload

--- hintUI.js
+++ hintUI.js
@@ -1,43 +1,40 @@
-function formatSeconds(seconds) {
-    if (
-        typeof seconds !== "number"
-        || isNaN(seconds)
-        || seconds < 0
-    ) {
-        return "";
-    }
-    if (seconds < 60) {
-        return `${seconds}s`;
-    }
-    const minutes = Math.floor(seconds / 60);
-    const remainingSeconds = seconds % 60;
-    return `${minutes}m ${remainingSeconds}s`;
-}
-
 module.exports = function (RED) {
     function HintUINode(config) {
         RED.nodes.createNode(this, config);
         var node = this;
-        var flowContext = node.context().flow;
-
-        flowContext.set("schedule", {}); // Initialize schedule in flow context
+        const nodeContext = node.context();
+        nodeContext.set("states", {}); // Start with empty state
 
         node.on('input', function (msg) {
             // Get individual entries for each node (max one per node)
-            let schedule = Object.values(flowContext.get("schedule") || {});
+            let state = nodeContext.get("states") || {};
+            if (msg.topic === "STATE") {
+                msg.payload.forEach(item => {
+                    if (!item.id) return; // Skip if no id is provided
+                    state[item.id] = {
+                        time: item.time,
+                        description: item.description,
+                        formatTime: item.formatTime
+                    };
+                });
+                nodeContext.set("states", state);
+            }
+
+            let schedule = Object.values(state);
 
             // Sort by time and take the next ones based on the number of outputs
             schedule.sort((a, b) => a.time - b.time);
 
-            console.log("Current schedule:", flowContext.get("schedule"));
             node.send(schedule
-                .concat(Array(config.outputs || 1).fill({ time: '', description: '' }))
+                .filter(item => isFinite(item.time) && item.time >= 0)
+                .concat(Array(config.outputs || 1)
+                    .fill({ time: Infinity, description: '' }))
                 .slice(0, config.outputs || 1)
                 .map(item => {
                     return {
                         topic: "HINT_SCHEDULE",
                         payload: {
-                            time: formatSeconds(item.time),
+                            time: item.formatTime || item.time,
                             description: item.description,
                         }
                     }
